048be78f57d54bfe8a8358f56885f04008b19c5f
farzan <farzanullah07@gmail.com>
2024-03-18 04:46:11 +0530

    client side done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import User from "./components/User";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      email: "george.bluth@reqres.in",
+      first_name: "George",
+      last_name: "Bluth",
+      avatar: "https://reqres.in/img/faces/1-image.jpg",
+    },
+    {
+      id: 2,
+      email: "janet.weaver@reqres.in",
+      first_name: "Janet",
+      last_name: "Weaver",
+      avatar: "https://reqres.in/img/faces/2-image.jpg",
+    },
+    {
+      id: 3,
+      email: "emma.wong@reqres.in",
+      first_name: "Emma",
+      last_name: "Wong",
+      avatar: "https://reqres.in/img/faces/3-image.jpg",
+    },
+    {
+      id: 4,
+      email: "eve.holt@reqres.in",
+      first_name: "Eve",
+      last_name: "Holt",
+      avatar: "https://reqres.in/img/faces/4-image.jpg",
+    },
+    {
+      id: 5,
+      email: "charles.morris@reqres.in",
+      first_name: "Charles",
+      last_name: "Morris",
+      avatar: "https://reqres.in/img/faces/5-image.jpg",
+    },
+    {
+      id: 6,
+      email: "tracey.ramos@reqres.in",
+      first_name: "Tracey",
+      last_name: "Ramos",
+      avatar: "https://reqres.in/img/faces/6-image.jpg",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+
+      <Container>
+        <Row>
+          {users.map((user, index) => (
+            <Col md={4} key={index}>
+              <User user={user} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
